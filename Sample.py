935b76a60634a235220776d4561f7e344c054b0c
RinalPatel <54743470+RinalPatel@users.noreply.github.com>
2022-11-08 11:41:58 +0530

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import flask
+
+
+# In[6]:
+
+
+
+import werkzeug
+import time
+import numpy as np
+import urllib
+import urllib.request
+import cv2
+import scipy.ndimage
+from flask import Flask,redirect,url_for,render_template
+from flask import send_file
+from flask import request
+import requests
+import base64
+import os
+import webbrowser
+from pathlib import Path
+from flask import send_from_directory
+
+
+# In[2]:
+
+
+app = flask.Flask(__name__)
+
+
+# In[3]:
+
+
+#to display the connection status
+@app.route('/', methods=['GET', 'POST'])
+def handle_call():
+    return "Hello you are Successfully Connected....."
+
+
+# In[4]:
+
+
+app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+# In[ ]:
+
+
+
+
